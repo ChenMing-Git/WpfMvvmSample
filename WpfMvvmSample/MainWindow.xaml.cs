41a74be4754fac6c34982fadc3cb3267a38152c3
ChenMing <attackaddress@gmail.com>
2022-08-12 09:48:49 +0800

    Add files via upload

---
+++ WpfMvvmSample/MainWindow.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows;
+using WpfMvvmSample.UI.ViewModel;
+
+namespace WpfMvvmSample
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            var menuViewModel = new MenuViewModel();
+            this.DataContext = menuViewModel;
+        }
+    }
+}
